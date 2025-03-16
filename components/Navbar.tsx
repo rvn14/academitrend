77279fb5e345f3266637f7444f55a0f4658ab1e8
rvn14 <dasunadithya123@gmail.com>
2025-03-16 10:04:20 +0530

    Predict page initiated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { BrainCircuit } from 'lucide-react'
 import React from 'react'
 import { Button } from './ui/button'
@@ -5,12 +6,12 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className='fixed bg-white/80 backdrop-blur-sm w-full h-20 flex items-center justify-between z-40 px-4 md:px-12'>
+    <div className='fixed bg-white/80 backdrop-blur-sm w-full h-20 flex items-center justify-between z-40 px-4 md:px-12 shadow-2xs font-poppins'>
         <div className='flex items-center gap-3'>
-            <div className="flex items-center gap-2">
+            <Link href={"/"} className="flex items-center gap-2">
               <BrainCircuit className="h-8 w-8 text-maroon-700" />
               <span className="text-xl font-bold text-gray-900">AcademiTrend</span>
-            </div>
+            </Link>
         </div>
         <div>
         <Button asChild className='bg-maroon-700 text-white'>
