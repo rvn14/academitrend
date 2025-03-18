0ef1286ad2159b423d723bb15374b3c3d5b8aec7
rvn14 <dasunadithya123@gmail.com>
2025-03-18 09:54:47 +0530

    Favicon updated

--- components/MyChart.tsx
+++ components/MyChart.tsx
@@ -154,7 +154,7 @@ export function Mychart() {
     <Card>
       <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
         <div className="grid flex-1 gap-1 text-center sm:text-left">
-          <CardTitle>Area Chart - Interactive</CardTitle>
+          <CardTitle>Area Chart</CardTitle>
           <CardDescription>
             Showing total visitors for the last 3 months
           </CardDescription>
